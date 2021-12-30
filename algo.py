e28f5ec661dcbe30b27a5539deb3cb8a0b7417f2
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-30 10:01:15 -0600

    remove debug print statements

--- algo.py
+++ algo.py
@@ -6,13 +6,11 @@ def accum(s):
     output = ""
     for letterIdx in range( len(s) ) :
         for idx in range( letterIdx+1 ) :
-            # print ( s[letterIdx] )
             ltr = s[letterIdx]
             if idx == 0 :
                 output += ltr.upper();
             else:
                 output += ltr.lower();
-            # print (ltr.upper())
         if letterIdx != (len(s)-1) :
             output += "-"
     return output
